4d6779f5ddc228c75f8ed1d8b922ee1e7bfc8cd5
Юлия Ларионова <yuliya_larionova@inbox.ru>
2016-12-24 18:42:13 +0300

    function is doing something one

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,7 +5,7 @@ from collections import Counter
 def load_data(filepath):
     if not os.path.exists(filepath):
         return None
-    with open (filepath) as file:
+    with open (filepath,encoding='utf-8') as file:
         input_data=file.read()
         return input_data
     
@@ -15,19 +15,25 @@ def get_list_of_words(data):
     all_words=re.findall(r'\w+',data.lower())    
     return all_words
         
-def get_most_frequent_words(data): 
-    number_frequent_words=10
+def get_number_words_repetitions(data): 
     number_of_words=Counter(data)
-    most_frequent_words=number_of_words.most_common(number_frequent_words)  
-    for word in most_frequent_words:
-        print(word[0],word[1])
+    return number_of_words
+ 
+
+def get_most_frequent_words(data):
+    print('10 самых часто встречаемых слов в данном тексте')
+    number_frequent_words=10
+    most_frequent_words=data.most_common(number_frequent_words)  
+    for word, number in most_frequent_words:
+        print(word,number)
         
 if __name__ == '__main__':
     filepath=input('Введите абсолютный путь до файла ')
-    text_strings=load_data(filepath)
-    if load_data(filepath) is not None:
-        wordlist=get_list_of_words(text_strings)
-        get_most_frequent_words(wordlist)
+    full_text=load_data(filepath)
+    if full_text is not None:
+        wordlist=get_list_of_words(full_text)
+        number_words_repetitions=get_number_words_repetitions(wordlist)
+        get_most_frequent_words(number_words_repetitions)
     else:
         print('Неверный путь до файла')
         
